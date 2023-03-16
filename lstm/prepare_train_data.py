cab0e9497dd2b205ee014fd6ca432773aacddff3
zhangchen <zhangchen@wealthgrow.cn>
2023-03-16 18:10:04 +0800

    1

--- lstm/prepare_train_data.py
+++ lstm/prepare_train_data.py
@@ -3,104 +3,110 @@ import pandas as pd
 import numpy as np
 from sklearn.preprocessing import MinMaxScaler
 import torch
+from torch import nn
 
 
 
 # 000905.SH 中证500
 from ts.stock_data_service import get_index_daily_price_as_df
 
+
+def normalize(list):
+    max_v, min_v = np.max(list), np.min(list)
+    # max_v = max(list)
+    # min_v = min(list)
+
+    list = (list - min_v) / (max_v - min_v)
+
+    return list
+
+
+
+class LSTM(nn.Module):
+    def __init__(self, input_size, hidden_size, output_size):
+        super().__init__()
+        self.input_size = input_size
+        self.hidden_size = hidden_size
+        self.output_size = output_size
+        self.num_directions = 1 # 单向LSTM
+        self.lstm = nn.LSTM(input_size, hidden_size)
+        self.linear = nn.Linear(self.hidden_size, self.output_size)
+
+    def forward(self, input_seq):
+        batch_size, seq_len = input_seq[0], input_seq[1]
+        h_0 = torch.randn(self.num_directions * self.num_layers, self.batch_size, self.hidden_size).to(device)
+        c_0 = torch.randn(self.num_directions * self.num_layers, self.batch_size, self.hidden_size).to(device)
+        # output(batch_size, seq_len, num_directions * hidden_size)
+        # todo 所以是先用lstm算一遍 再linear一下，得到pred值?
+        output, _ = self.lstm(input_seq, (h_0, c_0))
+        pred = self.linear(output)
+        pred = pred[:, -1, :]
+        return pred
+
+
+
+
+
+
+
 # 每一行代表一天 y代表预测值 y之前的每一列数据代表一个维度
 def create_train_data(index_code):
     index_nav_df = get_index_daily_price_as_df(index_code, 'SSE')
 
-    # 创建训练集
-    days_before = 10
+    # 只需要最近5年的
+    index_nav_df = index_nav_df[['close','chg', 'vol']]
+    index_nav_df = index_nav_df[-5*365:]
 
-    series = index_nav_df['close'].copy()
+    chg_list = index_nav_df['chg'].tolist()
+    vol_list = index_nav_df['vol'].tolist()
 
-    train_series, test_series = series[:500], series[490:]
+    chg_list = normalize(chg_list)
+    vol_list = normalize(vol_list)
 
-    train_data = pd.DataFrame()
+    # 构造输入数据和验证数据
+    train_seq = []
+    real_seq = []
 
-    # 通过移位，创建历史 days_before 天的数据
-    for i in range(10):
-        # 当前数据的 7 天前的数据，应该取 开始到 7 天前的数据； 昨天的数据，应该为开始到昨天的数据，如：
-        # [..., 1,2,3,4,5,6,7] 昨天的为 [..., 1,2,3,4,5,6]
-        # 比如从 [2:-7+2]，其长度为 len - 7
-        train_data['c%d' % i] = train_series.tolist()[i: -days_before + i]
+    for i in range(0, len(chg_list) - 16):
+        train_seq_2 = []
+        for j in range(i, i+15):
+            train_seq_2.append([chg_list[j], vol_list[j]])
 
-    # 获取对应的 label
-    train_data['y'] = train_series.tolist()[days_before:]
+        train_seq.append(train_seq_2)
+        real_seq.append([chg_list[i+15]])
 
-    # 是否生成 index
-    print(1)
-
-
-
-
-
-    seq_number = np.array(
-        [112., 118., 132., 129., 121., 135., 148., 148., 136., 119., 104.,
-         118., 115., 126., 141., 135., 125., 149., 170., 170., 158., 133.,
-         114., 140., 145., 150., 178., 163., 172., 178., 199., 199., 184.,
-         162., 146., 166., 171., 180., 193., 181., 183., 218., 230., 242.,
-         209., 191., 172., 194., 196., 196., 236., 235., 229., 243., 264.,
-         272., 237., 211., 180., 201., 204., 188., 235., 227., 234., 264.,
-         302., 293., 259., 229., 203., 229., 242., 233., 267., 269., 270.,
-         315., 364., 347., 312., 274., 237., 278., 284., 277., 317., 313.,
-         318., 374., 413., 405., 355., 306., 271., 306., 315., 301., 356.,
-         348., 355., 422., 465., 467., 404., 347., 305., 336., 340., 318.,
-         362., 348., 363., 435., 491., 505., 404., 359., 310., 337., 360.,
-         342., 406., 396., 420., 472., 548., 559., 463., 407., 362., 405.,
-         417., 391., 419., 461., 472., 535., 622., 606., 508., 461., 390.,
-         432.], dtype=np.float32)
-
-    # 行转换成列
-    seq_number = seq_number[:, np.newaxis]
-
-    # print(repr(seq))
-    # 1949~1960, 12 years, 12*12==144 month
-    # 结果 seq_year_month 有两列，列0代表year从0开始，列1代表month从0开始   like (0,0) (0,1) ... (0,11) (1,0) ...
-    seq_year = np.arange(12)
-    seq_month = np.arange(12)
-    seq_year_month = np.transpose(
-        [np.repeat(seq_year, len(seq_month)),
-         np.tile(seq_month, len(seq_year))],
-    )  # Cartesian Product
-
-    # 简单的merge
-    seq = np.concatenate((seq_number, seq_year_month), axis=1)
-
-    # 如上的作用是形成  price, year, month 的三元组合
-
-    # normalization
-    seq = (seq - seq.mean(axis=0)) / seq.std(axis=0)
+    # todo torch.cuda.FloatTensor
 
 
+    # input_size, hidden_size, output_size, batch_size
+    # model = LSTM(2, 32, 1, batch_size=args.batch_size).to(device)
+    model = LSTM(2, 32, 1)
+    # loss_function = nn.MSELoss().to(device)
+    loss_function = nn.MSELoss()
 
-    # inp_dim 训练的维度
-    inp_dim = 3
-    # mid_dim 三个门的宽度?
-    mid_dim = 3
-    # num_layers 是使用两个LSTM对数据进行预测，然后将他们的输出堆叠起来。
-    num_layers = 2
-    rnn = torch.nn.LSTM(inp_dim, mid_dim, num_layers)
 
-    input = torch.randn(seq_len, batch_size, inp_dim)
-    output = rnn(input)
+    for i in range(0, len(train_seq)):
+        print(f"{i}/{len(train_seq)}")
+
+        model.zero_grad()
+
+        train_ = train_seq[i]
+        real_ = real_seq[i]
+
+        train_ = torch.FloatTensor(train_)
+        real_ = torch.FloatTensor(real_).view(-1)  # view -1 拉平到1维
+
+        score = model(train_)
+        loss = loss_function(score, real_)
+
+        loss.backward()
+        optimizer.step()
+
 
     print(1)
 
 
 
-def eg1():
-
-    # 标准化
-    scaler = MinMaxScaler(feature_range=(-1, 1))
-    train_data_normalized = scaler.fit_transform(train_data.reshape(-1, 1))
-
-    # PyTorch模型是使用张量训练的。要将数据集转换为张量，我们可以简单地将数据集传递给FloatTensor对象的构造函数
-    train_data_normalized = torch.FloatTensor(train_data_normalized).view(-1)
 
 
 
