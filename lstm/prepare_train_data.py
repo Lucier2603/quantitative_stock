86692f24971312b4ce037c9ce7263c3e6d743639
zhangchen <zhangchen@wealthgrow.cn>
2023-03-21 21:56:42 +0800

    1

--- lstm/prepare_train_data.py
+++ lstm/prepare_train_data.py
@@ -18,27 +18,28 @@ def normalize(list):
     max_v, min_v = np.max(list), np.min(list)
     list = (list - min_v) / (max_v - min_v)
 
-    return list
+    return [float(x) for x in list]
 
 
 seq = 15
 
-class RegLSTM(nn.Module, x_dim, y_dim):
-    def __init__(self):
+class RegLSTM(nn.Module):
+    def __init__(self, x_dim, y_dim):
         super(RegLSTM, self).__init__()
 
         # batch_first=True的意义
         # DataLoader返回数据时候一般第一维都是batch，pytorch的LSTM层默认输入和输出都是batch在第二维。
         # lstm要求的入参顺序为（seq_len , btach_size , input_size）  seq是输入序列的长度 在这里应该是15  batch_size应该是总样本数量
 
-        self.lstm = nn.LSTM(input_size=x_dim, output_size=y_dim, hidden_size=32, num_layers=1, batch_first=True)
+        self.lstm = nn.LSTM(input_size=x_dim, hidden_size=32, num_layers=1, batch_first=True)
         # 输入格式是1，输出隐藏层大小是32
         # 对于小数据集num_layers不要设置大，否则会因为模型变复杂而导致效果会变差
         # num_layers顾名思义就是有几个lstm层，假如设置成2，就相当于连续经过两个lstm层
         # 原来的输入格式是：(seq, batch, shape)
         # 设置batch_first=True以后，输入格式就可以改为：(batch, seq, shape)，更符合平常使用的习惯
         # todo 为什么是32*seq
-        self.linear = nn.Linear(32 * seq, 1)
+        # Linear 参数  mid_dim, out_dim
+        self.linear = nn.Linear(32 * seq, y_dim)
 
     def forward(self, x):
         x, (h, c) = self.lstm(x)
@@ -80,14 +81,17 @@ def create_train_data(index_code):
         l = normalize(l)
         x_raw_data[x_name] = l
 
+    y_raw_data = [float(x) for x in y_raw_data]
+
 
 
     # step 3.1 构造输入数据和验证数据
+    print('step 3.1 build data')
     x_list = []
     y_list = []
     seq = 15
 
-    for i in range(0, len(index_nav_df)):
+    for i in range(0, len(index_nav_df) - seq):
         xp = []
 
         # 一个样本有seq条数据点
@@ -115,12 +119,14 @@ def create_train_data(index_code):
 
     X_train = torch.tensor(x_train).reshape(-1, seq, x_dim).to(device)
     y_train = torch.tensor(y_train).reshape(-1, y_dim).to(device)
+    X_test = torch.tensor(x_test).reshape(-1, seq, x_dim).to(device)
+    y_test = torch.tensor(y_test).reshape(-1, y_dim).to(device)
 
 
+    # step 4.1 训练
+    print('step 4.1 start train')
     model = RegLSTM(x_dim, y_dim).to(device)
-    loss_function = nn.MSELoss().to(device)
-    # loss_function = nn.MSELoss()
-    optimizer = torch.optim.Adam(model.parameters(), lr=1e-2)
+    loss_fun = nn.MSELoss().to(device)
 
     model = model.to(device)
     optimizer = torch.optim.Adam(model.parameters(), lr=0.003)
@@ -134,7 +140,7 @@ def create_train_data(index_code):
         loss.backward()
         optimizer.step()
         if epoch % 20 == 0 and epoch > 0:
-            test_loss = loss_fun(model(test_X_train), test_y_train)
+            test_loss = loss_fun(model(X_test), y_test)
             print("epoch:{}, loss:{}, test_loss: {}".format(epoch, loss, test_loss))
 
 
